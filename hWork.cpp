bf4f8d17c47eba5f6871e837169c6eca6c2c2118
sadeval <162736837+sadeval@users.noreply.github.com>
2024-03-10 19:48:32 +0200

    hWork.cpp

--- hWork.cpp
+++ hWork.cpp
@@ -9,8 +9,8 @@ void interval(double* a, double* b, double* dist, double* byte_dist) {
 
 int main() {
 
-    double a = 51;
-    double b = 255;
+    double a = 51.0;
+    double b = 255.0;
     double res = 0;
     double byte_res = 0; 
 
