2591bb49c7ff1a508e4ba8d2ca8a8b8231747ba6
sadev <sadev@DESKTOP-5TK1OND>
2024-03-10 15:11:48 +0200

    Добавьте файлы проекта.

---
+++ hWork.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+
+void interval(double* a, double* b, double *dist, double* byte_dist) {
+
+    *dist = *b - *a;
+    *byte_dist = (char*)&b - (char*)&a;
+}
+
+
+
+int main() {
+
+    double a = 51.6;
+    double b = 255.4;
+    double res = 0;
+    double byte_res = 0; 
+
+    interval(&a, &b, &res, &byte_res);
+
+    cout << "Distance between a and b: " << res << "\n";
+    cout << "Distance between a and b in bytes: " << byte_res << " b" << "\n";
+    
+    return 0;
+
+}
